dab8e3328de2ac794ab091b9f691b74e5bc9592e
Patrik Persson <patrik.persson@cs.lth.se>
2022-07-29 16:54:53 +0200

    bugfix: corrected encoding for encryption example

--- crack/src/CodeBreakingExample.java
+++ crack/src/CodeBreakingExample.java
@@ -8,8 +8,8 @@ public class CodeBreakingExample {
     public static void main(String[] args) throws InterruptedException {
 
         // 'N' from a public key, and an encrypted message.
-        BigInteger n = new BigInteger("217158619938165709");
-        String ciphertext = "59gp9p5qfd1-14z7kbsnuo1g-76btlfh7wwq-12po84bg8dvl-drtiw0a91nt-4ooagwi5kxh-k1o2l6iyyvm-14wbyot0llud-1d2rd5vcoczr";
+        BigInteger n = new BigInteger("113546829441971119");
+        String ciphertext = "2o1k5jnv4x102gqilsqmp194dwj6k4nu31ycsrg6oo9407oi83447b3kpsuckqeey2nrenev0dv8qoxbpyonlbyojk8opwrgb1h0lt7zr39ygsb35aky0hlqz624yninojlkhij2vapt85qafyxmyeo8hxx0g2el2bd57qufnmwkkv0t";
 
         ProgressTracker tracker = new Tracker();
         String plaintext = Factorizer.crack(ciphertext, n, tracker);

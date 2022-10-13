f23030a6a44a55a7c0a58379d9fd403cfdf07df3
bjorklundjonathan <jonathan.bjorklund123@gmail.com>
2022-10-13 13:38:50 +0200

    Lite mer på 5

--- wash/src/wash/control/SpinController.java
+++ wash/src/wash/control/SpinController.java
@@ -2,13 +2,17 @@ package wash.control;
 
 import actor.ActorThread;
 import wash.io.WashingIO;
+import wash.control.WashingMessage;
+import wash.control.WashingMessage.Order;
 
 public class SpinController extends ActorThread<WashingMessage> {
 
     // TODO: add attributes
+    WashingIO io;
+    boolean left = true;
 
     public SpinController(WashingIO io) {
-        // TODO
+        this.io = io;
     }
 
     @Override
@@ -20,10 +24,29 @@ public class SpinController extends ActorThread<WashingMessage> {
             while (true) {
                 // wait for up to a (simulated) minute for a WashingMessage
                 WashingMessage m = receiveWithTimeout(60000 / Settings.SPEEDUP);
+                ActorThread<WashingMessage> sender = m.getSender();
 
                 // if m is null, it means a minute passed and no message was received
+                
                 if (m != null) {
                     System.out.println("got " + m);
+                    switch(m.getOrder()) {
+                        case SPIN_SLOW:
+                            if(left) {
+                                io.setSpinMode(WashingIO.SPIN_LEFT);
+                                left = false;
+                            } else io.setSpinMode(WashingIO.SPIN_RIGHT);
+                            break;
+                        case SPIN_FAST:
+                            io.setSpinMode(WashingIO.SPIN_FAST);
+                            break;
+                        case SPIN_OFF:
+                            io.setSpinMode(WashingIO.SPIN_IDLE);
+                            break;
+                        default:
+                            break;
+                    }
+                    sender.send(new WashingMessage(this, Order.ACKNOWLEDGMENT));
                 }
                 
                 // ... TODO ...
